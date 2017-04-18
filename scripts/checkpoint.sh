1befe314d9162ab4b3a03976c880bc7738f0da03
SaurabhSakpal <sfstoe@gmail.com>
2017-04-18 15:38:28 -0400

    Partial Checkpointing script

--- scripts/checkpoint.sh
+++ scripts/checkpoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# getting inputs from the command line
+# getting arguments from the command line
 if [ ! $# -eq 8 ]; then
 	echo "USAGE: sudo ./checkpoint.sh -c CONTAINER_ID -d DESTINATION_IP -u USER -n CHECKPOINT_NAME
 EXAMPLE: sudo ./checkpoint.sh -c hkj3434ljl43 -u ssakpal -n first -d 10.10.3.7"
@@ -38,9 +38,23 @@ fi
 
 #checkpoint a container
 start=`date +%s%N`
-echo $CONTAINER_ID
-echo $DESTINATION_IP
-echo $CHECKPOINT_NAME
+time docker checkpoint create --checkpoint-dir $DIRECTORY $CONTAINER_ID $CHECKPOINT_NAME
 end=`date +%s%N`
 runtime=$((end-start))
 echo "Execution Time : $runtime nanoseconds"
+
+#exporting the file system
+TAR_NAME="/home/$USER/$CHECKPOINT_NAME.tar"
+time docker export $CONTAINER_ID > $TAR_NAME
+
+#SCP file system to DESTINATION_IP
+SCP_LOCATION="$USER@$DESTINATION_IP:/home/$USER"
+time scp -r $TAR_NAME  $SCP_LOCATION
+
+
+#SCP checkpoint file to the DESTINATION_IP
+CHECKPOINT_LOCATION="$DIRECTORY/$CHECKPOINT_NAME/"
+SCP_LOCATION="$USER@$DESTINATION_IP:$DIRECTORY"
+time scp -r $CHECKPOINT_LOCATION $SCP_LOCATION
+
+#clean up script
