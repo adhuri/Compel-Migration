e2e3a19aec523f0fdeab6b2b1326630fb44e1104
SaurabhSakpal <sfstoe@gmail.com>
2017-04-19 16:11:55 -0400

    Adding maps to the server object

--- server/model/Model.go
+++ server/model/Model.go
@@ -4,8 +4,42 @@ import "sync"
 
 type Server struct {
 	sync.RWMutex
+	previousMigrationMap map[string]int64
+	migrationStatus      map[string]bool
 }
 
 func NewServer() *Server {
 	return &Server{}
+}
+
+func (server *Server) GetPreviousContainerMigrationTime(containerId string) int64 {
+	server.RLock()
+	defer server.RUnlock()
+	timestamp, present := server.previousMigrationMap[containerId]
+	if present {
+		return timestamp
+	}
+	return 0
+}
+
+func (server *Server) GetContainerMigrationStatus(containerId string) bool {
+	server.RLock()
+	defer server.RUnlock()
+	status, present := server.migrationStatus[containerId]
+	if present {
+		return status
+	}
+	return false
+}
+
+func (server *Server) SetContainerMigrationStatus(containerId string, status bool) {
+	server.Lock()
+	defer server.Unlock()
+	server.migrationStatus[containerId] = status
+}
+
+func (server *Server) SetPreviousContainerMigrationTime(containerId string, timestamp int64) {
+	server.Lock()
+	defer server.Unlock()
+	server.previousMigrationMap[containerId] = timestamp
 }
