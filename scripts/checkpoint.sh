392d7e15d2b5e151f5dbfa92b2fa94172365679e
SaurabhSakpal <sfstoe@gmail.com>
2017-04-18 20:47:39 -0400

    Adding execution time calculation for all the commands

--- scripts/checkpoint.sh
+++ scripts/checkpoint.sh
@@ -38,25 +38,38 @@ fi
 
 #checkpoint a container
 start=`date +%s%3N`
-time docker checkpoint create --checkpoint-dir $DIRECTORY $CONTAINER_ID $CHECKPOINT_NAME
+docker checkpoint create --checkpoint-dir $DIRECTORY $CONTAINER_ID $CHECKPOINT_NAME
 end=`date +%s%3N`
 runtime=$((end-start))
-echo "Execution Time : $runtime nanoseconds"
+echo "Execution Time : $runtime milliseconds"
 
 #exporting the file system
+start=`date +%s%3N`
 TAR_NAME="/home/$USER/$CHECKPOINT_NAME.tar"
 time docker export $CONTAINER_ID > $TAR_NAME
+end=`date +%s%3N`
+echo "Execution Time : $runtime milliseconds"
 
 #SCP file system to DESTINATION_IP
+start=`date +%s%3N`
 SCP_LOCATION="$USER@$DESTINATION_IP:/home/$USER"
 time scp -r $TAR_NAME  $SCP_LOCATION
+end=`date +%s%3N`
+echo "Execution Time : $runtime milliseconds"
 
 
 #SCP checkpoint file to the DESTINATION_IP
+start=`date +%s%3N`
 CHECKPOINT_LOCATION="$DIRECTORY/$CHECKPOINT_NAME/"
 SCP_LOCATION="$USER@$DESTINATION_IP:$DIRECTORY"
 time scp -r $CHECKPOINT_LOCATION $SCP_LOCATION
+end=`date +%s%3N`
+echo "Execution Time : $runtime milliseconds"
+
 
 #clean up
+start=`date +%s%3N`
 docker rm $CONTAINER_ID
 rm $TAR_NAME
+end=`date +%s%3N`
+echo "Execution Time : $runtime milliseconds"
